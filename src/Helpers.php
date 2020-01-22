795e8780f153c7d9b2e75bc72413fe22156d143c
samaphp <samaphp@gmail.com>
2020-01-22 20:41:21 +0300

    New class for helpers.

---
+++ src/Helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Samaphp\Desktime;
+
+/**
+ * Class Helpers.
+ */
+class Helpers extends DesktimeClass {
+
+  /**
+   * Constructs a new object.
+   */
+  public function __construct() {
+    parent::__construct();
+  }
+
+}
